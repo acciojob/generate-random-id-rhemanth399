8aaf59fe5a410ccca340442d7194d4554fdcaae5
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-06-14 17:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+	let a=''
+  let letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z']
+  for(let i=0;i<l;i++)
+  {
+     
+   a+=letters[Math.floor(Math.random()*letters.length)]
+  
+  }
+  
+   return a;
 }
 
 // Do not change the code below.
